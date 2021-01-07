b24b8a74d12419996f6f749c7369ad3c0b07e260
Haneul-Yoo <skysky2387@gmail.com>
2021-01-07 14:58:32 +0900

    Assign port

--- main.py
+++ main.py
@@ -212,5 +212,4 @@ def task_done():
 
 init_paths()
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000")
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
